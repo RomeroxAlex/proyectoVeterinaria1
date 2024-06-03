3b2806729ff59c2a52f43303bcf9ce19f1a0898c
cr146 <cr146@DESKTOP-JVQTV4E>
2024-06-02 23:02:02 -0600

    bellaca

--- src/Clases/prueba.java
+++ src/Clases/prueba.java
@@ -5,6 +5,6 @@ import javax.swing.JOptionPane;
 public class prueba {
     public static void main(String[] args) {
         
-        JOptionPane.showMessageDialog(null, "Hola bella");
+        JOptionPane.showMessageDialog(null, "Bellaca");
     }
 }
